6a9a817bb72232008b85b60c1fc9e34f9b0e0d7f
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-02 21:57:48 -0700

    Simplify Vite + React landing page

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -16,18 +14,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Portfolio Insights</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          This API frontend will be built using Vite and React. Coming soon...
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
