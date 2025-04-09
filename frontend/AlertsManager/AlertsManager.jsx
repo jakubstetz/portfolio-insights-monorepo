33785b72d55bd3ecac633f289ea992a63e81f46e
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-08 18:36:33 -1000

    Create AlertsManager component

---
+++ frontend/AlertsManager/AlertsManager.jsx
@@ -0,0 +1,14 @@
+import './AlertsManager.css';
+import Alert from '../Alert/Alert';
+
+function AlertsManager() {
+  return (
+    <div id="alerts-manager">
+      <Alert message="AAPL > $200" />
+      <Alert message="TSLA < $150" />
+      {/* More Alert components would be mapped here in real usage */}
+    </div>
+  );
+}
+
+export default AlertsManager;
