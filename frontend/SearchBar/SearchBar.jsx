e23d86d44725e95842dd18cc3711bb0226e01649
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-08 17:13:39 -1000

    Create SearchBar component

---
+++ frontend/SearchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+import './SearchBar.css';
+
+function SearchBar() {
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="Search stock..." />
+    </div>
+  );
+}
+
+export default SearchBar;
