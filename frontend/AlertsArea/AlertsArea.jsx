e7a788d9ff3571fa01e6d4855054dcf5c3cba891
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-10 12:19:12 -1000

    Implement communication between SearchBar, AlertsArea, AlertsManager, and backend API

--- frontend/AlertsArea/AlertsArea.jsx
+++ frontend/AlertsArea/AlertsArea.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react'
 import './AlertsArea.css';
 import SearchBar from '../SearchBar/SearchBar';
 import AlertsManager from '../AlertsManager/AlertsManager';
 
 function AlertsArea() {
+  const [alerts, setAlerts] = useState('')
+
+  const searchHandler = async search_term => {
+    const trimmed = search_term.trim().toUpperCase();
+    const isValidTicker = /^$|^[A-Z]{1,10}$/.test(trimmed);
+  
+    if (!isValidTicker) {
+      setAlerts('')
+    } else {
+      const api_response = await fetch(`http://127.0.0.1:8000/alerts?search_term=${trimmed}`);
+      const retrieved_alerts = await api_response.json();
+      setAlerts(retrieved_alerts);
+    }
+  };
+
   return (
     <div id="alerts-area">
-      <SearchBar />
-      <AlertsManager />
+      <SearchBar searchHandler={searchHandler} />
+      <AlertsManager alerts={alerts}/>
     </div>
   );
 }
