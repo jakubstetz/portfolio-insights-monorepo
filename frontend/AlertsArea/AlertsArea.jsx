68c8cee76f8ec2bf7b9dc1af744e8d2d83810e71
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-10 23:09:50 -0700

    Implement default rendering of all alerts in databes in AlertsManager component on mount

--- frontend/AlertsArea/AlertsArea.jsx
+++ frontend/AlertsArea/AlertsArea.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './AlertsArea.css';
 import SearchBar from '../SearchBar/SearchBar';
 import AlertsManager from '../AlertsManager/AlertsManager';
 
 function AlertsArea({onNewAlert}) {
-  const [alerts, setAlerts] = useState('')
+  const [alerts, setAlerts] = useState([])
+
+  useEffect(() => { // Load all alerts on mount
+    const fetchAlerts = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/alerts');
+        const data = await response.json();
+        setAlerts(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+  
+    fetchAlerts();
+  }, []);
 
   const searchHandler = async search_term => {
     const trimmed = search_term.trim().toUpperCase();
--- frontend/AlertsManager/AlertsManager.jsx
+++ frontend/AlertsManager/AlertsManager.jsx
@@ -2,7 +2,6 @@ import './AlertsManager.css';
 import Alert from '../Alert/Alert';
 
 function AlertsManager({alerts, onNewAlert}) {
-  alerts = [[5,"NVDA",157.88,"above","2025-04-08T13:57:28.721610-04:00",null,false,null,false,"2025-06-10T15:00:00-04:00"],[1,"AAPL",155.0,"above","2025-04-08T03:35:45.646194-04:00",null,true,"2025-04-08T14:38:12.944709-04:00",null,null], [3,"SPY",122.37,"above","2025-04-08T03:35:45.646194-04:00",null,true,"2025-04-08T14:38:15.381968-04:00",null,null]]
 
   return (
     <div id="alerts-manager">
